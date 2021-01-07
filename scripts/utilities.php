f537a3dd068e36fc47ee89ddc4a7d2985708b67c
namePlayer <namePlayer@users.noreply.github.com>
2021-01-07 12:44:56 +0100

    Add getUserId() function

--- scripts/utilities.php
+++ scripts/utilities.php
@@ -7,3 +7,11 @@ function isLoggedIn(): bool
     }
     return false;
 }
+
+function getUserId(): int
+{
+    if(isset($_SESSION['uptimeLoggedIn']) && $_SESSION['uptimeLoggedIn'] > 0) {
+        return $_SESSION['uptimeLoggedIn'];
+    }
+    return 0;
+}
