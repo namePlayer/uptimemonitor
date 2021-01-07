4d0aaba32ba72c1e83b46328019c7ea7f60f9d80
namePlayer <namePlayer@users.noreply.github.com>
2021-01-07 12:44:06 +0100

    Move session Start

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
-session_start();
-
-$messages = [];
-
 error_reporting(-1);
 ini_set('error_reporting', 1);
 ini_set('display_startup_errors', 1);
+
+session_start();
+
+$messages = [];
 
 require_once __DIR__.'/config/base.php';
 require_once SCRIPT_DIR.'/database.php';
